6827278b1daaac956d1b108c10d6cecd2ea6b062
Charles Bendernagel <cbendernagel@gmail.com>
2017-04-07 14:01:52 -0400

    added .jars

---
+++ Danfango/src/java/Services/MovieService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author charles
+ */
+public interface MovieService extends JpaRepository<Movie, Integer> {  
+}
